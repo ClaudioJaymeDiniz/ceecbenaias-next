4bc45d9639cb6bf3a0e4813ae2aef80c9a906dbc
claudiojayme <claudiojayme@gmail.com>
2025-08-15 15:35:07 -0300

    alterando hero novamente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Users, Music, Dumbbell, Brain, Heart, X } from "lucide-react";
 import { useState } from "react";
 import perfil from "@/assets/images/img9.jpg";
-import hero from "@/assets/images/logo-fundo.jpeg";
+import hero from "@/assets/images/capa-hero.jpg";
 import Footer from "@/components/footer/footer";
 
 import Header from "@/components/header/header";
